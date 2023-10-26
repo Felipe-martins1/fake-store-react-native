99ca507243b6112ff61ec321f9858e9477d68e51
Felipe <Felipe.dom.martins@gmail.com>
2023-10-26 20:05:19 -0300

    feat: home page

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,7 +1,12 @@
-import { TextInput, View, StyleSheet, FlatList, Image } from 'react-native';
+import { View, StyleSheet, FlatList, Image, Text } from 'react-native';
 import { CategoriesList } from '@src/components/CategoriesList';
 import { ProductCard } from '@src/components/ProductCard';
+
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { productsService } from '@src/services/products.service';
+import { categoriesService } from '@src/services/categories.service';
 
 const MAIN_HERO_IMAGE_URI =
   'https://images.unsplash.com/photo-1605171399454-f2a0e51b811b?auto=format&fit=crop&q=80&w=1931&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
@@ -9,60 +14,61 @@ const MAIN_HERO_IMAGE_URI =
 export function HomeScreen() {
   const [category, setCategory] = useState<string | undefined>(undefined);
 
+  const { data: categories } = useQuery({
+    queryKey: ['categories'],
+    queryFn: () => categoriesService.findAll(),
+    initialData: [],
+  });
+
+  const { data, isFetching, isError } = useQuery({
+    queryKey: ['products', { category }],
+    queryFn: () => productsService.findAll({ category }),
+    initialData: [],
+  });
+
+  function renderItem({ item }: any) {
+    return (
+      <ProductCard
+        product={item}
+        onPressAddToCart={() => {}}
+        style={{
+          width: '48%',
+        }}
+      />
+    );
+  }
+
+  function renderListHeaderComponent() {
+    return (
+      <View style={styles.listHeaderContainer}>
+        <Image
+          style={{ height: 130, borderRadius: 8 }}
+          source={{
+            uri: MAIN_HERO_IMAGE_URI,
+          }}
+        />
+        <CategoriesList categories={categories} selected={category} onSelect={setCategory} />
+      </View>
+    );
+  }
+
+  function renderListEmptyComponent() {
+    if (isFetching) return <Text>Loading...</Text>;
+
+    if (isError) return <Text>Oops...An unexpected error occurred</Text>;
+
+    return <Text>No products found</Text>;
+  }
+
   return (
     <FlatList
       numColumns={2}
       columnWrapperStyle={{ justifyContent: 'space-between' }}
       contentContainerStyle={styles.container}
-      ListHeaderComponent={() => (
-        <View style={styles.listHeaderContainer}>
-          <TextInput placeholder="Search" style={styles.input} cursorColor="gray" />
-          <Image
-            style={{ height: 130, borderRadius: 8 }}
-            source={{
-              uri: MAIN_HERO_IMAGE_URI,
-            }}
-          />
-          <CategoriesList
-            categories={['Celulares', 'Fones de Ouvido', 'Carregadores', 'Cabos', 'Capinhas']}
-            selected={category}
-            onSelect={setCategory}
-          />
-        </View>
-      )}
-      data={[
-        {
-          id: 1,
-          title: 'Sample Product',
-          image: 'sample-image.jpg',
-          price: '19.99',
-        },
-        {
-          id: 2,
-          title: 'Sample Product',
-          image: 'sample-image.jpg',
-          price: '19.99',
-        },
-        {
-          id: 3,
-          title: 'Sample Product',
-          image: 'sample-image.jpg',
-          price: '19.99',
-        },
-        {
-          id: 4,
-          title: 'Sample Product',
-          image: 'sample-image.jpg',
-          price: '19.99',
-        },
-        {
-          id: 5,
-          title: 'Sample Product',
-          image: 'sample-image.jpg',
-          price: '19.99',
-        },
-      ]}
-      renderItem={({ item }) => <ProductCard product={item} onPress={() => {}} />}
+      ListHeaderComponent={renderListHeaderComponent}
+      ListEmptyComponent={renderListEmptyComponent}
+      data={!isError ? data : []}
+      renderItem={renderItem}
     />
   );
 }
@@ -76,12 +82,5 @@ const styles = StyleSheet.create({
   listHeaderContainer: {
     flexDirection: 'column',
     gap: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.05)',
-    backgroundColor: 'white',
-    borderRadius: 8,
-    padding: 10,
   },
 });
