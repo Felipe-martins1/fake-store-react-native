5e00d667e7cda929248a6e018d6135bfff4a074f
Felipe <Felipe.dom.martins@gmail.com>
2023-10-26 20:06:20 -0300

    feat: react query and axios libs

--- App.tsx
+++ App.tsx
@@ -2,11 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { Routes } from '@src/navigation';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <StatusBar />
+        <Routes />
+      </QueryClientProvider>
     </NavigationContainer>
   );
 }
