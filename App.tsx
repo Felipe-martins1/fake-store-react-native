cf67172a51bacf279a387c0e2a603b36c716c342
Felipe <Felipe.dom.martins@gmail.com>
2023-10-26 21:04:02 -0300

    feat: add cart provider to app

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Routes } from '@src/navigation';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CartProvider } from '@src/context/CartContext';
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ export default function App() {
     <NavigationContainer>
       <QueryClientProvider client={queryClient}>
         <StatusBar />
-        <Routes />
+        <CartProvider>
+          <Routes />
+        </CartProvider>
       </QueryClientProvider>
     </NavigationContainer>
   );
